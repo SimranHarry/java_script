81c9b9cd22607ae13a05ef46b779ea59478f2d2b
SimranHarry <146826083+SimranHarry@users.noreply.github.com>
2023-10-10 14:12:28 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+const studentId=1
+var studentName="Simran"
+let studentAge="25"
+studentEmail="simran@gmail.com"
+let studentEmailPass;
+
+
+// studentId=2
+console.log(studentId);
+
+/*
+    prefer not to use var
+    becsuse of issue in block scope and functional scope
+
+*/
+
+console.table([studentId,studentName,studentAge,studentEmail,studentEmailPass])
---
+++ 02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// atert("3+3")  //we are using node JS not browser
+
+console.log(3
+    +3) // code readability should be high
+
+console.log("Simran")
+
+let name= "Simran"
+let age= 25
+let isLoggedIn="False"
+
+// number = 2 to power 54
+// bigint
+// string =""
+// boolean = true/false
+// null 
+// undefined = stand alone value
+// symbol = unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+console.log(typeof 25);
+console.log(typeof age);
